cc5030e1839b7f3ea62d1183a4ab3d7406c5d0bb
RangerMauve <RangerMauve@htomail.com>
2014-11-18 22:31:35 -0500

    Added comments, onadd, onremove

--- example/example.js
+++ example/example.js
@@ -2,23 +2,27 @@ var MQTTEmitter = require("../");
 
 var events = new MQTTEmitter();
 
-events.on("user/+name/message/#path", function(data, params) {
-	var name = params.name;
-	var path = params.path;
-	console.log(name, "@", "/" + path.join("/"), ":", data);
-});
-
+var topic = "user/+name/message/#path";
 var messages = ["hi", "what's up?", "Hey I'm new.", "How about that weather, eh?", "I know"];
 var names = ["Bob", "Angelina", "xX420xNoSc0peXx"];
 var count = 10;
+var delay = 200;
+
+events.on(topic, handle_message);
+say_something();
+
+function handle_message(data, params) {
+	var name = params.name;
+	var path = params.path;
+	console.log(name, "@", "/" + path.join("/"), ":", data);
+}
 
 function say_something() {
 	var event = "user/" + random_from_list(names) + "/message/lobby";
 	events.emit(event, random_from_list(messages));
-	if (count--) setTimeout(say_something, 1000);
+	if (count--) setTimeout(say_something, delay);
+	else events.removeListener(topic, handle_message);
 }
-
-say_something();
 
 function random_from_list(list) {
 	return list[Math.floor(Math.random() * list.length)];
